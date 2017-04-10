49c805fe5cc93d5939540cbe91c7aa4cb821b8e3
isaacNo1 <cxx7821@gmail.com>
2017-04-10 22:03:23 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , only: [:new]
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
