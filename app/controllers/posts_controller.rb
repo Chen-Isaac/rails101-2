7d90b684a3034541cffd30031ce7db44b42a9d7c
isaacNo1 <cxx7821@gmail.com>
2017-04-17 23:05:27 +0800

    add post edit and delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
-
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -19,7 +19,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path, alert:"Post deleted"
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    
+  end
 
   def post_params
     params.require(:post).permit(:content)
